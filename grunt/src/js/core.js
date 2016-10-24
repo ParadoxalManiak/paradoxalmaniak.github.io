561f688c3901b2ebf5c4786dac140b978eb9a6e5
ParadoxalManiak <devil28tare@gmail.com>
2016-10-24 23:04:05 +0300

    Changed some files

---
+++ grunt/src/js/core.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    console.log("Core script loaded!");
+
+    if (!window.jQuery) {
+        console.log("jQuery is NOT loaded!");
+    }
+};
+
+// JS Core code
+(function ($){
+    "use strict";
+    
+})(jQuery);
